f4fe81a727c8fa064ded126dec0c3e6d33f2b685
david <dgrogan@chromium.org>
2019-10-03 11:03:34 -0700

    use --start-fullscreen instead of --kiosk

--- kiosk.sh
+++ kiosk.sh
@@ -4,4 +4,6 @@ xset -dpms
 xset s off
 openbox-session &
 rm -rf ~/.{config,cache}/chromium
-chromium-browser --kiosk --no-first-run  'file:///home/dgrogan/j/j.html'
+#chromium-browser --kiosk --no-first-run  'file:///home/dgrogan/j/j.html'
+# --start-fullscreen is as if someone hit F11 right after startup
+chromium-browser --start-fullscreen --no-first-run  'file:///home/dgrogan/j/j.html'
